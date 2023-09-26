01451eceafb3f565d6f11734d9751faa45d17083
MUNAZZA BAWANY <munazzabawany@MUNAZZAs-Air.lan>
2023-09-26 18:11:11 -0500

    Added code

--- code/shakespeare.py
+++ code/shakespeare.py
@@ -9,73 +9,67 @@ OUTPUT_DIR = "artifacts"
 OUTPUT_PATH = os.path.join(OUTPUT_DIR, "shakespeare_report.csv")
 
 NUM_LINES_TO_SKIP = 246
-LAST_LINE_START = "End of this Etext"
+LAST_LINE_STOP = 124437
 
+with open(STOPWORDS_PATH, 'r') as in_file:
 
-def load_stopwords():
+    header = in_file.readline()
+    text = in_file.read()
+
+clean_stopwords = re.sub('[^A-Za-z\s]', '', text)
+
+def load_stopwords(stopwords):
     """Load the stopwords from the file and return a set of the cleaned stopwords."""
 
-    stopwords = set()
+    sw = set()
+    for word in stopwords.split('\n'):
+        sw.add(word.lower())
+    return sw
+stopwords_set = load_stopwords(clean_stopwords)
 
-    # fill this in
+with open(SHAKESPEARE_PATH, 'r') as in_file2:
+	text2 = in_file2.readlines()[NUM_LINES_TO_SKIP:LAST_LINE_STOP]
+	
+dirty_text = ""
+should_read = True
 
-    return stopwords
+for line in text2:
+	if line.startswith("<<"):
+		should_read = False
+	if should_read:
+		dirty_text += line
+	if line.endswith(">>\n"):
+		should_read = True
 
+text_without_punctuation = re.sub('[^A-Za-z\s]', '', dirty_text)
+text_without_spaces = re.sub("\s+", " ", text_without_punctuation)
+clean_shakespeare = text_without_spaces.lower()
 
-def load_shakespeare_lines():
-    "Loads every line in the dataset that was written by Shakespeare as a list of strings."
+def word_count(str_):
+	word_count = {}
+	for word in clean_shakespeare.split(" "):
+		if word in stopwords_set:
+			continue
+		if word in word_count:
+			word_count[word] += 1
+		else:
+			word_count[word] = 1
+	return word_count
 
-    shakespeare_lines = []
+shakespeare_wordcount = word_count(clean_shakespeare)
 
-    # fill this in
+tuples_wordcounts = shakespeare_wordcount.items()
+sorted_wordcounts = sorted(tuples_wordcounts, key = lambda x:x[1], reverse = True)
 
-    return shakespeare_lines
+def write_wordcounts(sorted_counts, path_):
 
+	with open(path_, 'w+') as out_file:
+		csv_writer = csv.writer(out_file)
 
-def get_shakespeare_words(shakespeare_lines):
-    """Takes the lines and makes a list of lowercase words."""
+		header = ['word', 'count']
+		data = sorted_wordcounts
 
-    # fill this in
-
-    return words
-
-
-def count_words(words, stopwords):
-    """Counts the words that are not stopwords.
-    returns a dictionary with words as keys and values."""
-
-    word_counts = dict()
-
-    # fill this in
-
-    return word_counts
-
-
-def sort_word_counts(word_counts):
-    """Takes a dictionary or word counts.
-    Returns a list of (word, count) tuples that are sorted by count in descending order."""
-
-    # fill this in
-
-    return sorted_word_counts
-
-
-def write_word_counts(sorted_word_counts, path):
-    """Takes a list of (word, count) tuples and writes them to a CSV."""
-
-       # fill this in
-
-
-if __name__ == "__main__":
-
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
-
-    stopwords = load_stopwords()
-
-    shakespeare_lines = load_shakespeare_lines()
-    shakespeare_words = get_shakespeare_words(shakespeare_lines)
-
-    word_counts = count_words(shakespeare_words, stopwords)
-    word_counts_sorted = sort_word_counts(word_counts)
-
-    write_word_counts(word_counts_sorted, OUTPUT_PATH)
+		csv_writer.writerow(header)
+		csv_writer.writerows(data)
+		
+write_wordcounts(sorted_wordcounts, OUTPUT_PATH)
